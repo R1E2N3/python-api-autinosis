adfaa4e2ee1f4ac5dac196df1f38cc5ffbdf2b37
R1E2N3 <78232754+R1E2N3@users.noreply.github.com>
2024-03-01 17:55:18 -0300

    Changing the app.py

--- api_request_test.py
+++ api_request_test.py
@@ -1,8 +1,22 @@
 import requests
 
-base_url = 'https://python-api-autinosis.onrender.com/uppercase'
+base_url = 'https://python-api-autinosis.onrender.com/predict_adult'
 
-params = {'text': 'hey there I\'m here'}
+params = {
+"Ethnicity": "",
+"jundice": 1,
+"A1": 1,
+"A2": 1,
+"A3": 0,
+"A4": 0,
+"A5": 0,
+"A6": 1,
+"A7": 0,
+"A8": 2,
+"A9": 0,
+"A10": 0,
+"Age": 10
+}
 
 response = requests.get(base_url, params)
 
--- app.py
+++ app.py
@@ -1,137 +1,50 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
-from flasgger import Swagger
-
-import book_review
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 api = Api(app)
-swagger = Swagger(app)
 
-class UppercaseText(Resource):
-    def get(self):
-        """
-        This method responds to the GET request for this endpoint and returns the data in uppercase.
-        ---
-        tags:
-        - Text Processing
-        parameters:
-            - name: text
-              in: query
-              type: string
-              required: true
-              description: The text to be converted to uppercase
-        responses:
-            200:
-                description: A successful GET request
-                content:
-                    application/json:
-                      schema:
-                        type: object
-                        properties:
-                            text:
-                                type: string
-                                description: The text in uppercase
-        """
-        text = request.args.get('text')
-
-        return jsonify({"text": text.upper()})
-    
-class Records(Resource):
-    def get(self):
-        """
-        This method responds to the GET request for returning a number of books.
-        ---
-        tags:
-        - Records
-        parameters:
-            - name: count
-              in: query
-              type: integer
-              required: false
-              description: The number of books to return
-            - name: sort
-              in: query
-              type: string
-              enum: ['ASC', 'DESC']
-              required: false
-              description: Sort order for the books
-        responses:
-            200:
-                description: A successful GET request
-                schema:
-                    type: object
-                    properties:
-                        books:
-                            type: array
-                            items:
-                                type: object
-                                properties:
-                                    title:
-                                        type: string
-                                        description: The title of the book
-                                    author:
-                                        type: string
-                                        description: The author of the book
-        """
-
-        count = request.args.get('count')  # Default to returning 10 books if count is not provided
-        sort = request.args.get('sort')
-
-        # Get all the books
-        books = book_review.get_all_records(count=count, sort=sort)
-
-        return {"books": books}, 200
-    
-class AddRecord(Resource):
-    def post(self):
-        """
-        This method responds to the POST request for adding a new record to the DB table.
-        ---
-        tags:
-        - Records
-        parameters:
-            - in: body
-              name: body
-              required: true
-              schema:
-                id: BookReview
-                required:
-                  - Book
-                  - Rating
-                properties:
-                  Book:
-                    type: string
-                    description: the name of the book
-                  Rating:
-                    type: integer
-                    description: the rating of the book (1-10)
-        responses:
-            200:
-                description: A successful POST request
-            400: 
-                description: Bad request, missing 'Book' or 'Rating' in the request body
-        """
-
-        data = request.json
-        print(data)
-
-        # Check if 'Book' and 'Rating' are present in the request body
-        if 'Book' not in data or 'Rating' not in data:
-            return {"message": "Bad request, missing 'Book' or 'Rating' in the request body"}, 400
-        # Call the add_record function to add the record to the DB table
-        success = book_review.add_record(data)
-
-        if success:
-            return {"message": "Record added successfully"}, 200
+def processes_data(question, value):
+    if question in ['A1', 'A2', 'A3', 'A4', 'A10', 'A5', 'A6', 'A7', 'A8', 'A9', 'A11', 'A12', 'A13']:
+        if value in ['1','2', 1, 2]:
+            return 1
         else:
-            return {"message": "Failed to add record"}, 500
-        
+            return 0
+    return value
 
+class PredictAdult(Resource):
+    def post(self):
+        # Load the model
+        model = joblib.load("adult.joblib")
 
-api.add_resource(AddRecord, "/add-record")
-api.add_resource(Records, "/records")
-api.add_resource(UppercaseText, "/uppercase")
+        # Check if the request contains JSON data
+        if request.is_json:
+            # Parse JSON data from the request body
+            data = request.get_json()
+
+            # Ensure the JSON data contains the expected keys
+            if all(key in data for key in ['Ethnicity', 'jundice', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'Age']):
+                # Perform data preprocessing
+                for key in data:
+                    data[key] = processes_data(key, data[key])
+
+                # Perform prediction with the loaded model
+                input_data = pd.DataFrame([data])
+                prediction = model.predict_proba(input_data)[0][1]
+                pred = round(prediction * 100)
+
+                return jsonify({"Result": pred})
+
+            else:
+                # If the JSON data does not contain the expected keys, return an error message
+                return jsonify({"error": "Invalid request format. Please provide all required fields."}), 400
+        else:
+            # If the request does not contain JSON data, return an error message
+            return jsonify({"error": "Request must contain JSON data."}), 400
+
+api.add_resource(PredictAdult, '/predict_adult')
 
 if __name__ == "__main__":
     app.run(debug=True)
