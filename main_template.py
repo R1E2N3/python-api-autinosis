ddd109ae38a660dad07ccb698d7af418833e2182
R1E2N3 <78232754+R1E2N3@users.noreply.github.com>
2024-03-01 21:20:36 -0300

    added adolescent and child endpoints

--- main_template.py
+++ main_template.py
@@ -44,7 +44,62 @@ class PredictAdult(Resource):
             # If the request does not contain JSON data, return an error message
             return jsonify({"error": "Request must contain JSON data."}), 400
 
+class PredictChild(Resource):
+    def post(self):
+        model = joblib.load('child.joblib')
+
+        if resquest.is_json:
+            data = request.get_json()
+
+            if all(key in data for key in ['Ethnicity', 'jundice', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'Age']):
+                # Perform data preprocessing
+                for key in data:
+                    data[key] = processes_data(key, data[key])
+
+                # Perform prediction with the loaded model
+                input_data = pd.DataFrame([data])
+                prediction = model.predict_proba(input_data)[0][1]
+                pred = round(prediction * 100)
+
+                return jsonify({"Result": pred})
+
+            else:
+                # If the JSON data does not contain the expected keys, return an error message
+                return jsonify({"error": "Invalid request format. Please provide all required fields."}), 400
+        else:
+            # If the request does not contain JSON data, return an error message
+            return jsonify({"error": "Request must contain JSON data."}), 400
+
+class PredictAdolescent(Resource):
+    def post(self):
+        model = joblib.load('adolescent.joblib')
+
+        if resquest.is_json:
+            data = request.get_json()
+
+            if all(key in data for key in ['Ethnicity', 'jundice', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'Age']):
+                # Perform data preprocessing
+                for key in data:
+                    data[key] = processes_data(key, data[key])
+
+                # Perform prediction with the loaded model
+                input_data = pd.DataFrame([data])
+                prediction = model.predict_proba(input_data)[0][1]
+                pred = round(prediction * 100)
+
+                return jsonify({"Result": pred})
+
+            else:
+                # If the JSON data does not contain the expected keys, return an error message
+                return jsonify({"error": "Invalid request format. Please provide all required fields."}), 400
+        else:
+            # If the request does not contain JSON data, return an error message
+            return jsonify({"error": "Request must contain JSON data."}), 400
+
+
 api.add_resource(PredictAdult, '/predict_adult')
+api.add_resource(PredictAdolescent, '/predict_adolescent')
+api.add_resource(PredictChild, '/predict_child')
 
 if __name__ == "__main__":
     app.run(debug=True)
